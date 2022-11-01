72634564ccba5aa7806c300176a12bdeaf2bfa06
Adedayo Adepoju <1mole.coco@gmail.com>
2022-11-01 22:02:36 +0100

    Initial commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,16 +6,16 @@ import requests
 def top_ten(subreddit):
     """ Returns: top ten post titles
         or None if queried subreddit is invalid """
-    
-    headers = {'User-Agent', '1mole'}
-    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
-    arguments = {'limit': 10}
 
-    response = requests.get(url, headers=headers, allow_redirects=False, arguments=arguments)
+    headers = {'User-Agent': '1mole'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
 
     if response.status_code == 200:
-        titles = response.json().get('data').get('children')
-        for i in titles:
-            print(i.get('data').get('title'))
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
     else:
         print(None)
